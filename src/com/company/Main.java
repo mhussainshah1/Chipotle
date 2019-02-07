f1ab71e781e84097b850306774383cb66fbd8f2f
mhussainshah1 <muhammadshah.14@sunymaritime.edu>
2019-02-07 15:10:07 -0500

    initial commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,15 +4,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
+    private double money;
 
     public static void main(String[] args) {
-
+        Main main = new Main();
+        String choice1,choice2,choice3,choice4,choice5,choice6,choice7,choice8,choice9 ,outPut="";
         List<String> rice = new ArrayList<>();
+        List<String> meat = new ArrayList<>();
+        List<String> beans = new ArrayList<>();
+        List<String> salsa = new ArrayList<>();
+        List<String> veggies = new ArrayList<>();
+        List<String> cheese = new ArrayList<>();
+        List<String> guacamole = new ArrayList<>();
+        List<String> queso = new ArrayList<>();
+        List<String> sourCream = new ArrayList<>();
+
         rice.add("white rice");
         rice.add("brown rice");
         rice.add("no rice");
 
-        List<String> meat = new ArrayList<>();
         meat.add("chicken");
         meat.add("steak");
         meat.add("carnidas");
@@ -20,108 +30,107 @@ public class Main {
         meat.add("sofritas");
         meat.add("veggies");
 
-        List<String> beans = new ArrayList<>();
         beans.add("pinto beans");
         beans.add("black beans");
         beans.add("no beans");
 
-        List<String> salsa = new ArrayList<>();
         salsa.add("mild salsa");
         salsa.add("medium salsa");
         salsa.add("hot salsa");
         salsa.add("all");
         salsa.add("no salsa");
 
-        List<String> veggies = new ArrayList<>();
         veggies.add("lettuce");
         veggies.add("fajita veggies");
         veggies.add("all");
         veggies.add("no veggies");
 
-        List<String> cheese = new ArrayList<>();
         cheese.add("chesse");
         cheese.add("no cheese");
 
-        List<String> guacamole = new ArrayList<>();
         guacamole.add("guacamole");
         guacamole.add("no guacamole");
 
-        List<String> queso = new ArrayList<>();
         queso.add("queso");
         queso.add("no queso");
 
-        List<String> sourCream = new ArrayList<>();
         sourCream.add("sour cream");
         sourCream.add("no sour cream");
 
-        String choice1,choice2,choice3,choice4,choice5,choice6,choice7,choice8,choice9 ,outPut="";
-
         for (int i = 1; i <26 ; i++) {
-            choice1 = getChoice(rice);
-            choice2 = getChoice(meat);
-            choice3 = getChoice(beans);
-            choice4 = getChoice(salsa);
-            choice5 = getChoice(veggies);
-            choice6 = getChoice(cheese);
-            choice7 = getChoice(guacamole);
-            choice8 = getChoice(queso);
-            choice9 = getChoice(sourCream);
-
-            double money = 3.0;
-            if(!choice1.equals("no rice")) {
-                money += 0.5;
-            }
-            if(!choice2.equals("")){
-                money += 0.5;
-            }
-            if (!choice3.equals("no beans")){
-                money +=0.5;
-            }
-            if(!choice4.equals("no salsa")){
-                money +=0.5;
-            }
+            main.setMoney(3);
+            choice1 = main.getChoice(rice);
+            choice2 = main.getChoice(meat);
+            choice3 = main.getChoice(beans);
+            choice4 = main.getChoice(salsa);
+            choice5 = main.getChoice(veggies);
+            choice6 = main.getChoice(cheese);
+            choice7 = main.getChoice(guacamole);
+            choice8 = main.getChoice(queso);
+            choice9 = main.getChoice(sourCream);
+            outPut = "";
             if(!choice5.equals("")){
-                money +=0.5;
-                outPut = choice5+", ";
+                outPut = choice5 + ", ";
             }
             if(!choice6.equals("")){
-                money +=0.5;
                 outPut += choice6 + ", ";
             }
             if(!choice7.equals("")){
-                money +=0.5;
                 outPut += choice7 + ", ";
             }
             if(!choice8.equals("")){
-                money +=0.5;
                 outPut += choice8 + ", ";
             }
             if(!choice9.equals("")){
-                money += 0.5;
                 outPut += choice9 + ", ";
             }
 
-            System.out.println("Burrito " +i +": " +choice1+", "+ choice2+ ", "+
-                    choice3+ ", "+choice4+ ", "+ outPut + "price = $" + money);
+            System.out.println("\nBurrito " +i +": " +choice1+", "+ choice2+ ", "+
+                    choice3+ ", "+choice4+ ", "+ outPut + "price = $" + main.getMoney()+"\n");
         }
     }
 
-    public static String getChoice(List<String> options){
+    public String getChoice(List<String> options){
         int random = (int)(Math.random() * options.size());
         String choice = options.get(random);
         if(choice.equals("all")){
             String choices = "";
             int index = options.indexOf("all");
-            //add all choice
+
+            //add all choices
             for(int i = 0; i < index - 1 ; i++){
-                choices += options.get(i) + ", ";;
+                choices += options.get(i) + ", ";
             }
             choices += options.get(index - 1 );
             choice = choices;
+            //System.out.println(index +" "+choice);
+            for (int i = 0; i < index; i++) {
+                addMoney(0.5);
+            }
+
         } else if(choice.equals("no veggies")||choice.equals("no cheese")|| choice.equals("no guacamole") || choice.equals("no queso")||choice.equals("no sour cream")){
-                choice = "";
+            choice = "";
+        } else if (choice.equals("no rice") || choice.equals("no beans")||choice.equals("no salsa")) {
+            //do nothing
+            choice = choice;
+        } else {
+            addMoney(0.5);
+        }
+        if(!choice.equals("")){
+            System.out.print(choice + "=" + getMoney() + ", ");
         }
         return choice;
     }
 
+    public double getMoney(){
+        return money;
+    }
+
+    public void setMoney(double money) {
+        this.money = money;
+    }
+
+    public void addMoney(double amount){
+        money += amount;
+    }
 }
